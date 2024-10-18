f72a0d47d9b504c0063390429591ca69ee466bc2
Balazs Komuves <bkomuves@gmail.com>
2024-10-18 15:20:58 +0200

    move the c files under the package subdirectory, so nimble copies them when installing the package...

--- goldilocks_hash/goldilocks.nim
+++ goldilocks_hash/goldilocks.nim
@@ -1,11 +1,11 @@
-{. compile: "../cbits/goldilocks.c" .}
+{. compile: "../goldilocks_hash/cbits/goldilocks.c" .}
 
 import ./types
 
-func neg* (x: F   ): F {. header: "../cbits/goldilocks.h", importc: "goldilocks_neg", cdecl .}
-func `+`* (x, y: F): F {. header: "../cbits/goldilocks.h", importc: "goldilocks_add", cdecl .}
-func `-`* (x, y: F): F {. header: "../cbits/goldilocks.h", importc: "goldilocks_sub", cdecl .}
-func `*`* (x, y: F): F {. header: "../cbits/goldilocks.h", importc: "goldilocks_mul", cdecl .}
+func neg* (x: F   ): F {. header: "../goldilocks_hash/cbits/goldilocks.h", importc: "goldilocks_neg", cdecl .}
+func `+`* (x, y: F): F {. header: "../goldilocks_hash/cbits/goldilocks.h", importc: "goldilocks_add", cdecl .}
+func `-`* (x, y: F): F {. header: "../goldilocks_hash/cbits/goldilocks.h", importc: "goldilocks_sub", cdecl .}
+func `*`* (x, y: F): F {. header: "../goldilocks_hash/cbits/goldilocks.h", importc: "goldilocks_mul", cdecl .}
 
 proc `+=`* (x: var F, y: F) = x = x + y
 proc `-=`* (x: var F, y: F) = x = x - y
